12c734f76f7e49f520b4889d1b5bf93aecb245db
vursen <mr.vursen@gmail.com>
2021-04-05 19:12:25 +0300

    Display the list of components in the default layout

--- src/layouts/x-default-layout.ts
+++ src/layouts/x-default-layout.ts
@@ -1,7 +1,8 @@
 import { html, css, customElement, internalProperty } from 'lit-element'
 import { MobxLitElement } from '@adobe/lit-mobx'
 
-import '@vaadin/vaadin-app-layout/vaadin-app-layout'
+import '@vaadin/vaadin-details'
+import '@vaadin/vaadin-app-layout'
 import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle'
 
 import { componentsStore } from '../stores/components-store.js'
@@ -26,6 +27,14 @@ export class XDefaultLayout extends MobxLitElement {
     await this.componentsStore.fetchComponents()
   }
 
+  get isLoading () {
+    return this.componentsStore.components.size === 0
+  }
+
+  get components () {
+    return [...this.componentsStore.components.values()]
+  }
+
   render () {
     return html`
       <vaadin-app-layout class="default-layout">
@@ -33,7 +42,25 @@ export class XDefaultLayout extends MobxLitElement {
 
         <h1 slot="navbar">Vaadin Stats</h1>
 
-        Components Count: ${this.componentsStore.components.size}
+        <ul slot="drawer">
+          <li>
+            <a href="#">Overview</a>
+          </li>
+
+          <vaadin-details>
+            <div slot="summary">
+              Components
+            </div>
+
+            <ul>
+              ${this.components.map((component) => html`
+                <li>
+                  <a href="#">${component.name}</a>
+                </li>
+              `)}
+            </ul>
+          </vaadin-details>
+        </ul>
 
         <slot></slot>
       </vaadin-app-layout>
