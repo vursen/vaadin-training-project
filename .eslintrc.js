0e2c883fdb989b18de0cf0cd6c7d6027fa504514
vursen <mr.vursen@gmail.com>
2021-04-06 09:54:43 +0300

    Install `eslint-config-vaadin`

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'vaadin',
+  rules: {
+    'no-unused-expressions': 'off',
+
+    'import/no-unresolved': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-extraneous-dependencies': 'off',
+
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
