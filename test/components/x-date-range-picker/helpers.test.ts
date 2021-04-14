00ab65c0109ec025a3896a206f1ed028bb18874d
vursen <mr.vursen@gmail.com>
2021-04-14 09:50:57 +0300

    Add more tests for the date range picker helpers

--- test/components/x-date-range-picker/helpers.test.ts
+++ test/components/x-date-range-picker/helpers.test.ts
@@ -9,7 +9,7 @@ import {
   deserializeDateRange,
 } from '../../../src/components/x-date-range-picker/helpers';
 
-describe('<x-date-range-picker /> helpers', () => {
+describe('x-date-range-picker helpers', () => {
   let clock: sinon.SinonFakeTimers;
 
   beforeEach(() => {
@@ -20,31 +20,62 @@ describe('<x-date-range-picker /> helpers', () => {
     clock.restore();
   });
 
-  it('should format date', () => {
-    const date = new Date();
+  describe('format', () => {
+    it('should format date', () => {
+      const date = new Date();
 
-    expect(format(date)).to.equal('2021-01-15');
+      expect(format(date)).to.equal('2021-01-15');
+    });
   });
 
-  it(`should return the today's date`, () => {
-    expect(today()).to.equal('2021-01-15');
+  describe('today', () => {
+    it(`should return the today's date`, () => {
+      expect(today()).to.equal('2021-01-15');
+    });
   });
 
-  it(`should substract a number of weeks ago from the today's date`, () => {
-    expect(weeksAgo(2)).to.equal('2021-01-01');
+  describe('weeksAgo', () => {
+    it(`should substract a number of weeks ago from the today's date`, () => {
+      expect(weeksAgo(2)).to.equal('2021-01-01');
+    });
   });
 
-  it('should serialize date range', () => {
-    const range = [weeksAgo(2), today()];
-    const expectedRange = '2021-01-01|2021-01-15';
+  describe('serializeDateRange', () => {
+    it('should serialize a date range', () => {
+      const range = [weeksAgo(2), today()];
+      const expectedRange = '2021-01-01|2021-01-15';
 
-    expect(serializeDateRange(range, '|')).to.equal(expectedRange);
+      expect(serializeDateRange(range, '|')).to.equal(expectedRange);
+    });
   });
 
-  it('should deserialize date range', () => {
-    const range = '2021-01-01|2021-01-15';
-    const expectedRange = [weeksAgo(2), today()];
+  describe('deserializeDateRange', () => {
+    it('should deserialize a date range', () => {
+      const range = '2021-01-01|2021-01-15';
+      const expectedRange = [weeksAgo(2), today()];
 
-    expect(deserializeDateRange(range, '|')).to.deep.equal(expectedRange);
+      expect(deserializeDateRange(range, '|')).to.deep.equal(expectedRange);
+    });
+
+    it('should deserialize an empty date range', () => {
+      const range = '';
+      const expectedRange = ['', ''];
+
+      expect(deserializeDateRange(range, '|')).to.deep.equal(expectedRange);
+    });
+
+    it('should deserialize a date range with there is only the start date', () => {
+      const range = '2021-01-01|';
+      const expectedRange = ['2021-01-01', ''];
+
+      expect(deserializeDateRange(range, '|')).to.deep.equal(expectedRange);
+    });
+
+    it('should deserialize a date range when there is only the end date', () => {
+      const range = '|2021-01-01';
+      const expectedRange = ['', '2021-01-01'];
+
+      expect(deserializeDateRange(range, '|')).to.deep.equal(expectedRange);
+    });
   });
 });
