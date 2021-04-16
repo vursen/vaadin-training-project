c9dee58c14096925a624bf17e30ebafd9db5e248
vursen <mr.vursen@gmail.com>
2021-04-16 09:25:33 +0300

    Add the date range picker on the components page

--- src/pages/x-component-page.ts
+++ src/pages/x-component-page.ts
@@ -1,8 +1,34 @@
-import { LitElement, html, customElement } from 'lit-element';
+import { LitElement, customElement, html, css } from 'lit-element';
+
+import { periodStore } from '../stores/period-store';
+
+import '../components/x-date-range-picker';
+import { XDateRangePickerValueChangedEvent } from '../components/x-date-range-picker';
 
 @customElement('x-component-page')
 export class XComponentPageElement extends LitElement {
+  static get styles() {
+    return css`
+      .wrapper {
+        padding: var(--lumo-space-m);
+      }
+    `;
+  }
+
+  onDateRangePickerValueChanged(event: XDateRangePickerValueChangedEvent) {
+    periodStore.setPeriod(event.detail.value);
+  }
+
   render() {
-    return html` <div class="component-page">Component Page</div> `;
+    return html`
+      <div class="wrapper">
+        <x-date-range-picker
+          class="date-range-picker"
+          .value="${periodStore.period}"
+          .ranges="${periodStore.periods}"
+          @value-changed="${this.onDateRangePickerValueChanged}"
+        ></x-date-range-picker>
+      </div>
+    `;
   }
 }
